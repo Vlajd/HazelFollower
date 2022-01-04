666a96508128ece99aefe6833eb136710ddc9057
Vlajd <boriskostadinov2005@gmail.com>
2022-01-04 18:01:29 +0100

    Logging Systen

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,4 +1,8 @@
 #pragma once
-#include "Hazel\Core.h"	
+
 #include "Hazel\Application.h"
+#include "Hazel\Log.h"
+
+// ---Entry Point------------ //
 #include "Hazel\EntryPoint.h"
+// -------------------------- //
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,9 +6,16 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
 
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello Log! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
+
+	return 0;
 }
 
 #endif

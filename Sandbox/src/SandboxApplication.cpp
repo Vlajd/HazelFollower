fa7ce91ae446a578649d1ba0200878bb6047a7ab
Vlajd <boriskostadinov2005@gmail.com>
2022-01-04 01:49:13 +0100

    Setup basic Application and Entry Point.

---
+++ Sandbox/src/SandboxApplication.cpp
@@ -0,0 +1,16 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application {
+
+	public:
+		Sandbox(){};
+		~Sandbox(){};
+};
+
+namespace Hazel {
+
+	Application* CreateApplication() {
+
+		return new Sandbox();
+	}
+}

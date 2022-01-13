2b7cf0c2efe94c297f1999ceb971d8d4462f3563
Vlajd <boriskostadinov2005@gmail.com>
2022-01-13 18:56:10 +0100

    modified:   Hazel/src/Hazel/Application.cpp 	modified:   Hazel/src/Hazel/Renderer/Buffer.h 	modified:   Hazel/src/Platform/OpenGL/OpenGLBuffer.h

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -11,12 +11,33 @@ namespace Hazel {
 
 	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+
+		switch (type) {
+
+			case Hazel::ShaderDataType::Float:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Float2:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Float3:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Float4:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Mat3:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Mat4:	return GL_FLOAT;
+			case Hazel::ShaderDataType::Int:	return GL_INT;
+			case Hazel::ShaderDataType::Int2:	return GL_INT;
+			case Hazel::ShaderDataType::Int3:	return GL_INT;
+			case Hazel::ShaderDataType::Int4:	return GL_INT;
+			case Hazel::ShaderDataType::Bool:	return GL_BOOL;
+		}
+
+		HZ_CORE_ASSERT(false, "ShaderDataTypeToOpenGLBaseType::Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application() {
-	
+
 		HZ_CORE_ASSERT(!s_Instance, "Application Already Exists!")
-		s_Instance = this;
+			s_Instance = this;
 
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
@@ -27,17 +48,42 @@ namespace Hazel {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
+		float vertices[3 * 7] = {
 
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+			-0.5f, -0.5f, 0.0f, 0.1f, 0.15f, 0.2f, 0.0f,
+			 0.0f,  0.5f, 0.0f, 0.1f, 0.1f, 0.7f, 0.25f,
+			 0.5f, -0.5f, 0.0f, 0.15f, 0.1f, 0.2f, 0.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();// glVertexAttribPointer
+		for (const auto& element : layout) {
+			
+			glEnableVertexAttribArray(index);
+
+			glVertexAttribPointer(index,						// glVertexAttribPointer
+				element.GetComponentCount(),					// glVertexAttribPointer
+				ShaderDataTypeToOpenGLBaseType(element.Type),	// glVertexAttribPointer
+				element.Normalized ? GL_TRUE : GL_FALSE,		// glVertexAttribPointer
+				layout.GetStride(),								// glVertexAttribPointer
+				(const void*)element.Offset						// glVertexAttribPointer
+			);													// glVertexAttribPointer
+
+			index++;
+		}
+
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -46,11 +92,14 @@ namespace Hazel {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main() {
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0f);
 			}
 		)";
@@ -61,9 +110,11 @@ namespace Hazel {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
-	
+			in vec4 v_Color;	
+
 			void main() {
-				color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+				//color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+				color = v_Color;
 			}
 		)";
 
